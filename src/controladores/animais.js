06c6d02e14f0b06687c7f93ff1e7413cdb294057
Lizstuart <lizstuart.nut.ufmg@gmail.com>
2023-04-05 19:53:34 -0300

    Adição de novos dados

--- src/controladores/animais.js
+++ src/controladores/animais.js
@@ -1,80 +1,80 @@
-const knex = require('../conexao');
+const knex = require("../conexao");
 
 const adicionarAnimal = async (req, res) => {
-    const { nome, especie, idade } = req.body
-    const { id } = req.usuario;
-    
-    if (!nome || !especie || !idade) {
-        return res.status(400).json('Todos os campos são obrigatórios')
-    }
+  const { nome, especie, raça, gênero, idade } = req.body;
+  const { id } = req.usuario;
 
-    try {
-        const novoAnimal = await knex('animais')
-            .insert({
-                nome,
-                especie,
-                idade,
-                id_usuario: id
-            })
-            .returning('*');
-        
-        return res.status(201).json(novoAnimal);
-        
-    } catch (error) {
-        return res.status(500).json('Erro no servidor');
-    }
-}
+  if (!nome || !especie || !idade) {
+    return res.status(400).json("Todos os campos são obrigatórios");
+  }
+
+  try {
+    const novoAnimal = await knex("animais")
+      .insert({
+        nome,
+        especie,
+        raça,
+        gênero,
+        idade,
+        id_usuario: id,
+      })
+      .returning("*");
+
+    return res.status(201).json(novoAnimal);
+  } catch (error) {
+    return res.status(500).json("Erro no servidor");
+  }
+};
 
 const editarAnimal = async (req, res) => {
-    const { nome, especie, idade } = req.body
-    let { id } = req.params;
-    id = Number(id);
-    
-    try {
-        const animalEditado = await knex('animais')
-            .update({ nome, especie, idade })
-            .where('id', id).returning('*');
-       
-        return res.status(202).json({ animalEditado });
-        
-    } catch (error) {
-        return res.status(500).json('Erro interno do servidor')
-    }
-}
+  const { nome, especie, raça, gênero, idade } = req.body;
+  let { id } = req.params;
+  id = Number(id);
+
+  try {
+    const animalEditado = await knex("animais")
+      .update({ nome, especie, raça, gênero, idade })
+      .where("id", id)
+      .returning("*");
+
+    return res.status(202).json({ animalEditado });
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
 
 const listarAnimais = async (req, res) => {
-    try {
-        const listaDeAnimais = await knex('animais')
-            .select('*')
-            .returning('*')
-        
-        return res.status(201).json(listaDeAnimais);
+  try {
+    const listaDeAnimais = await knex("animais").select("*").returning("*");
 
-    } catch (error) {
-        return res.status(500).json('Erro interno do servidor!')
-    }
-}
+    return res.status(201).json(listaDeAnimais);
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor!");
+  }
+};
 
 const deletarAnimal = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const { rowCount } = await knex('animais').delete().where('id', id).returning('id');
+  try {
+    const { rowCount } = await knex("animais")
+      .delete()
+      .where("id", id)
+      .returning("id");
 
-        if (rowCount < 1) {
-            return res.status(404).json('Usuário não encontrado')
-        }
-
-        return res.status(200).json()
-
-    } catch (error) {
-        return res.status(500).json('Erro interno do servidor');
+    if (rowCount < 1) {
+      return res.status(404).json("Usuário não encontrado");
     }
-}
+
+    return res.status(200).json();
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
 
 module.exports = {
-    adicionarAnimal,
-    editarAnimal,
-    listarAnimais,
-    deletarAnimal
-}
+  adicionarAnimal,
+  editarAnimal,
+  listarAnimais,
+  deletarAnimal,
+};
--- src/controladores/tutores.js
+++ src/controladores/tutores.js
@@ -1,64 +1,61 @@
-const knex = require('../conexao');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const senhaJwt = require('../senhaJwt');
-
+const knex = require("../conexao");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const senhaJwt = require("../senhaJwt");
 
 const adicionarUsuario = async (req, res) => {
-    const { nome, email, senha } = req.body
-    
-    try {
-        const senhaCriptografada = await bcrypt.hash(senha, 10);
+  const { nome, email, senha } = req.body;
 
-        const novoUsuario = await knex('usuarios')
-            .insert({
-                nome,
-                email,
-                senha: senhaCriptografada
-            })
-            .returning('*')
-        
-        return res.status(201).json(novoUsuario)
-        
-    } catch (error) {
-        return res.status(500).json('Erro interno do servidor!')
-    }
-}
+  try {
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+    const novoUsuario = await knex("usuarios")
+      .insert({
+        nome,
+        email,
+        senha: senhaCriptografada,
+      })
+      .returning("*");
+
+    return res.status(201).json(novoUsuario);
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor!");
+  }
+};
 
 const login = async (req, res) => {
-    const { email, senha } = req.body;
+  const { email, senha } = req.body;
 
-    if (!email || !senha) {
-        return res.status(404).json('Email e senha são obrigatórios')
+  if (!email || !senha) {
+    return res.status(404).json("Email e senha são obrigatórios");
+  }
+
+  try {
+    const dadosUsuario = await knex("usuarios").where({ email }).first();
+
+    if (dadosUsuario.rowCount < 1) {
+      return res.status(404).json("Email ou senha inválidos");
     }
 
-    try {
-        const dadosUsuario = await knex("usuarios").where({ email }).first();
+    const validarSenha = await bcrypt.compare(senha, dadosUsuario.senha);
 
-        if (dadosUsuario.rowCount < 1) {
-            return res.status(404).json('Email ou senha inválidos')
-        }
-
-        const validarSenha = await bcrypt.compare(senha, dadosUsuario.senha)
-
-        if (!validarSenha) {
-            return res.status(400).json('Email ou senha inválidos')
-        }
-
-        const token = jwt.sign({ id: dadosUsuario.id }, senhaJwt, {
-            expiresIn: '8h'
-        });
-
-        const { senha: _, ...loginDoUsuario } = dadosUsuario;
-
-        return res.status(201).json({ dadosUsuario: loginDoUsuario, token });
-
-    } catch (error) {
-        return res.status(500).json('Erro interno do servidor')
+    if (!validarSenha) {
+      return res.status(400).json("Email ou senha inválidos");
     }
-}
+
+    const token = jwt.sign({ id: dadosUsuario.id }, senhaJwt, {
+      expiresIn: "8h",
+    });
+
+    const { senha: _, ...loginDoUsuario } = dadosUsuario;
+
+    return res.status(201).json({ dadosUsuario: loginDoUsuario, token });
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
 
 module.exports = {
-    adicionarUsuario,
-    login
-}
+  adicionarUsuario,
+  login,
+};
